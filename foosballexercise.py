7496beb8f7d7b2e5e87b7228986620a195a9edb9
nestorbalce <nestorbalce@gmail.com>
2020-05-18 17:01:10 -0700

    List Exercise - Foosball
    From the Python Essentials from Lighthouse Labs Thinkific Free Course

---
+++ foosballexercise.py
@@ -0,0 +1,46 @@
+#foosball exercise
+index = 0
+foosballers = [
+  "Mia",
+  "Reta",
+  "Augustine",
+  "Jin",
+  "Waylon",
+  "Jamel",
+  "Alphonso",
+  "Sage",
+  "Hubert",
+  "Raymon",
+  "Rebecca",
+  "Monty",
+  "Glen",
+  "Christi",
+  "Patrice",
+  "Craig",
+  "Dexter",
+  "Wally",
+  "Ian",
+  "Pat"
+]
+
+print(foosballers[0:3])
+print(foosballers[9:12])
+print(foosballers[-11])
+print(foosballers[8:13])
+
+print(foosballers)
+
+newplayers=[
+  "Walt",
+  "Jesse",
+  "Saul",
+  "Mike",
+  "Gus"
+]
+
+print(newplayers)
+
+newteam = foosballers + newplayers
+
+print(newteam)
+
