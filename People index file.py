d4f71aaa8420c9f9066c17639fbc9a95a28f06e1
nestorbalce <nestorbalce@gmail.com>
2020-05-30 10:19:32 -0700

    Add files via upload

---
+++ People index file.py
@@ -0,0 +1,17 @@
+hotDogQueue = [
+  "Guy with Stroller",
+  "Girl with baloons",
+  "Guy with tophat",
+  "Girl with normal temperature dog",
+  "hard hat guy"
+]
+
+print(hotDogQueue.index("Guy with Stroller"))
+
+print(hotDogQueue.index("Girl with baloons"))
+
+print(hotDogQueue.index("Guy with tophat"))
+
+print(hotDogQueue.index("Girl with normal temperature dog"))
+
+print(hotDogQueue.index("hard hat guy"))
